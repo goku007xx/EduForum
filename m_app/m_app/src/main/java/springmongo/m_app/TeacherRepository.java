dc58c7d1f19f3c6a90b99ded97ae84bd984ee6a9
thespes <gurugkgurukiran@gmail.com>
2022-04-11 21:29:34 +0530

    all clear

--- m_app/m_app/src/main/java/springmongo/m_app/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package springmongo.m_app;
-
-import java.util.Optional;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TeacherRepository extends MongoRepository<Teacher, String> {
-	Optional<Teacher> findTeacherByEmail(String email);
-}
