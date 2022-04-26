b49afe98eb725f01a1ff7864c11b75e9e68ed64a
goku007xx <gokulkarthik12345@gmail.com>
2022-04-26 10:56:39 +0530

    Accessing teacher's subjects list

--- EduForums/src/main/java/com/example/EduForums/subject/SubjectRepository.java
+++ EduForums/src/main/java/com/example/EduForums/subject/SubjectRepository.java
@@ -1,10 +1,14 @@
 package com.example.EduForums.subject;
 
+import java.util.List;
 import java.util.Optional;
+
+import com.example.EduForums.teacher.Teacher;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface SubjectRepository extends MongoRepository<Subject, String> {
 	Optional<Subject> findSubjectBySubjectCode(String subjectCode);
+	List<Subject> findSubjectBySubjectTeacher(Teacher teacher);
 
 }
